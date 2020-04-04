2818420d11e60758d3f8fccb45e45b43b2f35f83
liupeng <liupeng.0@outlook.com>
2020-04-04 15:09:51 +0800

    补充README.md文件 V 1.0.2

--- src/main/java/cn/liupengstudy/ordworld/controller/IndexController.java
+++ src/main/java/cn/liupengstudy/ordworld/controller/IndexController.java
@@ -20,17 +20,33 @@ import java.util.Date;
 @RequestMapping("/indexCationPaper")
 public class IndexController {
 
+    /**
+     * @描述
+     * @参数  [name]
+     * @返回值  java.lang.String
+     * @创建人  liupeng
+     * @作者联系方式 LIUPENG.0@outlook.com
+     * @创建时间  2020/4/4 - 3:07 下午
+     * @修改人和其它信息
+     */
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    String home(String name) {
+        return "Hello " + name + "!";
     }
 
+    /**
+     * @描述
+     * @参数  []
+     * @返回值  java.lang.String
+     * @创建人  liupeng
+     * @作者联系方式 LIUPENG.0@outlook.com
+     * @创建时间  2020/4/4 - 3:06 下午
+     * @修改人和其它信息
+     */
     @RequestMapping("/now")
     String hehe() {
         return "现在时间：" + (new Date()).toLocaleString();
     }
 
 }
-
-
 

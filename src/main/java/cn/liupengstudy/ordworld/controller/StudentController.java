3be4f543b77786f3ee2561384fcaae80c24c8731
liupeng <liupeng.0@outlook.com>
2020-04-11 22:46:02 +0800

    学生管理系统.学生注册 V 1.3.4

--- src/main/java/cn/liupengstudy/ordworld/controller/StudentController.java
+++ src/main/java/cn/liupengstudy/ordworld/controller/StudentController.java
@@ -28,9 +28,6 @@ public class StudentController {
 
     /**
      * 通过主键查询单条数据
-     *
-     * @param id 主键
-     * @return 单条数据
      */
     @ApiOperation(value = "通过学ID查询学生")
     @RequestMapping(path = "/selectOne", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
@@ -89,18 +86,32 @@ public class StudentController {
     @ApiOperation(value = "学生注册")
     @RequestMapping(path = "/addOne", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public LPR addOne(@RequestBody Student student) {
+        student.setPhonenumber(student.getPhonenumber().replace(" ", ""));
+        student.setStudentid(student.getStudentid().replace(" ", ""));
         LPR lpr = new LPR();
         lpr.setWhat("学生注册");
         boolean key = true;
-        Student student1 = this.studentService.insert(student);
-        if (student1 == null) {
+        LPR idLpr = this.selectByStudentID(student);
+        if (idLpr.isReturnKey()) {
             key = false;
-            lpr.setWhy("注册失败");
+            lpr.setWhy("注册失败, 该学号已注册");
         } else {
-            lpr.setWhy("注册成功");
+            LPR phoneNumbwerLpr = this.selectByPhoneNumber(student);
+            if (phoneNumbwerLpr.isReturnKey()) {
+                key = false;
+                lpr.setWhy("注册失败, 联系方式重复");
+            } else {
+                Student student1 = this.studentService.insert(student);
+                if (student1 == null) {
+                    key = false;
+                    lpr.setWhy("注册失败");
+                } else {
+                    lpr.setWhy("注册成功");
+                }
+                lpr.setReturnObject(student1);
+            }
         }
         lpr.setReturnKey(key);
-        lpr.setReturnObject(student1);
         return lpr;
     }
 
